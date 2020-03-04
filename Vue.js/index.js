54a330d23c3ba1928b627f0a16ee770cf0b213df
David <davidsegu9@gmail.com>
2020-03-04 10:34:57 -0600

    for in vue.js

--- Vue.js/index.js
+++ Vue.js/index.js
@@ -8,7 +8,19 @@ var app = new Vue({
         return {
             name: 'Bitcoin',
             img:'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            cangePercent:100
+            cangePercent:100,
+            prices:[1231,5498,5498,548,5568,684,9684,98219,-48,947,60,984, ],
+
+            pricesWithDays: [
+                { day: 'Lunes', value: 8400 },
+                { day: 'Martes', value: 7900 },
+                { day: 'Miercoles', value: 8200 },
+                { day: 'Jueves', value: 9000 },
+                { day: 'Viernes', value: 9400 },
+                { day: 'Sabado', value: 10000 },
+                { day: 'Domingo', value: 10200 },
+            ]
+
         }
     }
 });
