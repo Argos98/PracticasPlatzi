de35ffee5da7367d035b0b2485344ac7a1965a41
David <davidsegu9@gmail.com>
2020-03-04 11:21:00 -0600

    class in vue.js

--- Vue.js/index.js
+++ Vue.js/index.js
@@ -7,9 +7,9 @@ var app = new Vue({
     data() {
         return {
             name: 'Bitcoin',
-            img:'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            cangePercent:100,
-            prices:[1231,5498,5498,548,5568,684,9684,98219,-48,947,60,984, ],
+            img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+            cangePercent: 100,
+            price: 8400,
 
             pricesWithDays: [
                 { day: 'Lunes', value: 8400 },
@@ -19,7 +19,19 @@ var app = new Vue({
                 { day: 'Viernes', value: 9400 },
                 { day: 'Sabado', value: 10000 },
                 { day: 'Domingo', value: 10200 },
-            ]
+            ],
+
+            showPrices : false
+
+
+        }
+    },
+
+    methods:{
+
+        toggleShowPrices(){
+            
+            this.showPrices = !this.showPrices
 
         }
     }
